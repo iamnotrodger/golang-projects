37e542d5456343ce3aa281d85286501a35905f63
iamnotrodger <rodger.retanal24@gmail.com>
2025-10-26 18:43:33 -0400

    fix(consumer): change Ticket price type from float64 to float32

--- services/consumer/internal/model/ticket.go
+++ services/consumer/internal/model/ticket.go
@@ -5,6 +5,6 @@ import "time"
 type Ticket struct {
 	ID        string    `json:"id,omitempty"`
 	Title     string    `json:"title"`
-	Price     float64   `json:"price"`
+	Price     float32   `json:"price"`
 	CreatedAt time.Time `json:"created_at"`
 }
